9061859e6666e349c2c85c712d7cabbedaa4951e
willigon106 <willigon106@gmail.com>
2019-02-03 14:56:44 -0500

    Se realiza el primer punto del ejercicio en cypress con lo siguiente:
    1. Crear una cuenta.
    2. Probar login correctamente.
    3. Crear una cuenta ya existente.

--- cypress/integration/estudiantes_punto1.js
+++ cypress/integration/estudiantes_punto1.js
@@ -1,12 +1,43 @@
-describe('Los estudiantes login', function() {
-    it('Visits los estudiantes and fails at login', function() {
+describe('Los estudiantes crear cuenta', function() {
+    it('Visita los estudiantes y crea nueva cuenta', function() {
         cy.visit('https://losestudiantes.co')
 		cy.contains('Cerrar').click()
-		//Lineas nuevas  
+		//Crear una nueva cuenta
 		cy.contains('Ingresar').click()
-		cy.get('.cajaLogIn').find('input[name="correo"]').click().type("wrongemail@example.com")
-		cy.get('.cajaLogIn').find('input[name="password"]').click().type("1234")
+		cy.get('.cajaSignUp').find('input[name="nombre"]').click().type("William")
+		cy.get('.cajaSignUp').find('input[name="apellido"]').click().type("González")
+		cy.get('.cajaSignUp').find('input[name="correo"]').click().type("wgpruebasautomaticas@gmail.com")
+		cy.get('.cajaSignUp').find('select[name="idPrograma"]').select('Ingeniería de Sistemas y Computación').should('have.value', '12')
+		cy.get('.cajaSignUp').find('input[name="password"]').click().type("12341234")
+		cy.get('.cajaSignUp').find('input[name="acepta"]').click()
+		cy.get('.cajaSignUp').contains('Registrarse').click()
+		cy.contains('Registro exitoso')
+    })
+})
+describe('Los estudiantes login', function() {
+    it('Visita los estudiantes y accede correctamente', function() {
+        cy.visit('https://losestudiantes.co')
+		cy.contains('Cerrar').click()
+		//Login Correcto
+		cy.contains('Ingresar').click()
+		cy.get('.cajaLogIn').find('input[name="correo"]').click().type("willigon106@gmail.com")
+		cy.get('.cajaLogIn').find('input[name="password"]').click().type("12341234")
 		cy.get('.cajaLogIn').contains('Ingresar').click()
-		cy.contains('El correo y la contraseña que ingresaste no figuran en la base de datos. Intenta de nuevo por favor.')
+    })
+})
+describe('Los estudiantes crear cuenta', function() {
+    it('Visita los estudiantes e intenta crear una cuenta que ya existe', function() {
+        cy.visit('https://losestudiantes.co')
+		cy.contains('Cerrar').click()
+		//Crear una cuenta ya existente
+		cy.contains('Ingresar').click()
+		cy.get('.cajaSignUp').find('input[name="nombre"]').click().type("Pepito")
+		cy.get('.cajaSignUp').find('input[name="apellido"]').click().type("Perez")
+		cy.get('.cajaSignUp').find('input[name="correo"]').click().type("wgpruebasautomaticas@gmail.com")
+		cy.get('.cajaSignUp').find('select[name="idPrograma"]').select('Ingeniería de Sistemas y Computación').should('have.value', '12')
+		cy.get('.cajaSignUp').find('input[name="password"]').click().type("12345678")
+		cy.get('.cajaSignUp').find('input[name="acepta"]').click()
+		cy.get('.cajaSignUp').contains('Registrarse').click()
+		cy.contains('Ya existe un usuario registrado con el correo')
     })
 })
