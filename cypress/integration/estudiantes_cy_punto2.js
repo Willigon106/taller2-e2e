a502f51613417e21c2523a5d89b630014735270e
willigon106 <willigon106@gmail.com>
2019-02-03 21:12:13 -0500

    Se crear la prueba del punto 3 de cypress con lo siguiente:
    1. Abre página de profesor por buscador.
    2. Abre página de profesor específico por filtro.
    3. Abre página de profesor por filtro con el primer dato.

--- cypress/integration/estudiantes_cy_punto2.js
+++ cypress/integration/estudiantes_cy_punto2.js
@@ -9,17 +9,16 @@ describe('Los estudiantes busqueda de profesores', function() {
 		cy.get('.Select-menu-outer').contains('Mario Eduardo Sanchez Puccini').click()
     })
 })
-describe('Los estudiantes busqueda de profesores', function() {
+describe('Los estudiantes busqueda de profesores uniandes', function() {
     it('Visita los estudiantes busca por filtro sin cambiar orden', function() {
         cy.visit('https://losestudiantes.co')
 		cy.contains('Cerrar').click()
 		//Buscar por filtros
 		cy.get('.uniandes').click()
 		cy.get('.jsx-2021299586').find('select[id="sample_select"]').select('Ingeniería de Sistemas y Computación').should('have.value', 'universidad-de-los-andes,pregrado,ingenieria-de-sistemas-y-computación')
-		cy.get('.profesores').find('div').first().find('a').click()
     })
 })
-describe('Los estudiantes busqueda de profesores', function() {
+describe('Los estudiantes busqueda de profesores uniandes', function() {
     it('Visita los estudiantes busca por filtro con cambiar orden', function() {
         cy.visit('https://losestudiantes.co')
 		cy.contains('Cerrar').click()
@@ -27,7 +26,15 @@ describe('Los estudiantes busqueda de profesores', function() {
 		cy.get('.uniandes').click()
 		cy.get('.jsx-2021299586').find('select[id="sample_select"]').select('Ingeniería de Sistemas y Computación').should('have.value', 'universidad-de-los-andes,pregrado,ingenieria-de-sistemas-y-computación')
 		cy.get('button').contains('Alfabético').click()
-		cy.get('.profesores').find('div').first().find('a').click()	
-		//Cuando se intenta cambiar el orden se queda pegado y no permite encontrar los profesores
+		cy.wait(2000)
+    })
+})
+describe('Los estudiantes busqueda de profesores unacional', function() {
+    it('Visita los estudiantes busca por filtro sin cambiar orden', function() {
+        cy.visit('https://losestudiantes.co')
+		cy.contains('Cerrar').click()
+		//Buscar por filtros
+		cy.get('.nacional').click()
+		cy.get('.jsx-2021299586').find('select[id="sample_select"]').select('Geología').should('have.value', 'universidad-nacional,pregrado,geologia')
     })
 })
---
+++ cypress/integration/estudiantes_cy_punto3.js
@@ -0,0 +1,40 @@
+describe('Los estudiantes abrir página de profesor', function() {
+    it('Visita los estudiantes abre página de profesor por buscador', function() {
+        cy.visit('https://losestudiantes.co')
+		cy.contains('Cerrar').click()
+		//Buscar en barra de búsqueda
+		//Profesor: Mario Eduardo Sanchez Puccini - Ingeniería de Sistemas
+		cy.get('.buscador').find('.Select').click()
+		cy.get('.Select-input').find("input").type('Mario', { force: true })
+		cy.get('.Select-menu-outer').contains('Mario Eduardo Sanchez Puccini').click()
+		cy.wait(3000)
+    })
+})
+describe('Los estudiantes abrir página de profesor', function() {
+    it('Visita los estudiantes abre página de profesor por filtro', function() {
+        cy.visit('https://losestudiantes.co')
+		cy.contains('Cerrar').click()
+		//Buscar por filtros
+		cy.get('.uniandes').click()
+		cy.get('.jsx-2021299586').find('select[id="sample_select"]').select('Ingeniería de Sistemas y Computación').should('have.value', 'universidad-de-los-andes,pregrado,ingenieria-de-sistemas-y-computación')
+		cy.get('button').contains('Alfabético').click()
+		cy.wait(2000)
+		cy.get('button').contains('Cargar más').click().wait(2000)
+		cy.get('button').contains('Cargar más').click().wait(2000)
+		cy.get('.profesores').find('div').find('a').contains('Mario Eduardo Sanchez Puccini').wait(2000).click()
+		cy.wait(3000)
+    })
+})
+describe('Los estudiantes abrir página de profesor', function() {
+    it('Visita los estudiantes abre página de profesor por filtro aleatoriamente primer dato', function() {
+        cy.visit('https://losestudiantes.co')
+		cy.contains('Cerrar').click()
+		//Buscar por filtros
+		cy.get('.uniandes').click()
+		cy.get('.jsx-2021299586').find('select[id="sample_select"]').select('Arquitectura').should('have.value', 'universidad-de-los-andes,pregrado,arquitectura')
+		cy.get('button').contains('Alfabético').click()
+		cy.wait(2000)
+		cy.get('.profesores').find('div').first().find('a').click()
+		cy.wait(3000)
+    })
+})
